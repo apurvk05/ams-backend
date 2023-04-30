22631f4d5f67c93518b19f55625537dff841c74e
Apurv Kumar <69334894+apurvk05@users.noreply.github.com>
2023-04-30 21:38:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const http = require('http')
-const socket =  require('socket.io')
+// const socket =  require('socket.io')
 const mongoose = require('mongoose')
 const passport = require('passport')
 const cors = require('cors')
@@ -11,7 +11,7 @@ dotenv.config()
 //MIDDILWARES
 const app = express();
 let server = http.createServer(app);
-let io = socket(server);
+// let io = socket(server);
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 app.use(cors())
@@ -29,21 +29,21 @@ require('./config/passport')(passport)
 
 app.use(morgan('dev'))
 
-io.on('connection', (socket) => {
-    socket.on('join room', ({room1, room2}) => {
-        socket.join(room1)
-        socket.join(room2)
-    })
-    socket.on("private message", (message) => {
-        io.to(message.room).emit('new Message', {
-            message: message.message,
-            sender: message.sender
-        });
-   })
-    socket.on('disconnect', function () {
-        console.log('Socket disconnected');
-    })
-})
+// io.on('connection', (socket) => {
+//     socket.on('join room', ({room1, room2}) => {
+//         socket.join(room1)
+//         socket.join(room2)
+//     })
+//     socket.on("private message", (message) => {
+//         io.to(message.room).emit('new Message', {
+//             message: message.message,
+//             sender: message.sender
+//         });
+//    })
+//     socket.on('disconnect', function () {
+//         console.log('Socket disconnected');
+//     })
+// })
 
 
 let _response = {}
